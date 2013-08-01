2d99e16fe4623e5bd9fd797070e65d98381a04f2
dpant <dpant@gmail.com>
2013-07-31 21:34:22 -0700

    Fix problem when we are going out of array length boundary while doing heapify

--- Heap/HeapSortWithCallBack.cpp
+++ Heap/HeapSortWithCallBack.cpp
@@ -1,7 +1,5 @@
 #include<iostream>
 using namespace std;
-
-
 /* 
 
 Heap is a **Tree structure** in which root nodes is greater than its child node.
@@ -19,6 +17,12 @@ void swap(int *x , int *y){
   *y= *x;
   *x = tmp;
 }
+void printArray(int *a, int len){
+  for(int i=0;i<len;i++){ // print
+	cout<< a[i]<< " ";
+  }
+  cout <<endl;
+}
 /* 
 Elements are out of order, not following heap property 
 if x is parent of y and x is smalle than y.
@@ -27,7 +31,7 @@ in that case we need to return TRUE;
 */
 
 int compare(int x,int y){
-  return x < y;
+  return x > y;
 }
 /* 
 
@@ -35,19 +39,25 @@ heapify the structure assuming the index { starting from 0 } is the one
 voilating the heap property 
 
 */
-void maxheapify(int *a,int len, int index,int (*cmp)(int x, int y)){
+int ifChildPresent(int ci,int len){
+  return ci < len;
+}
+void heapify(int *a,int len, int index,int (*cmp)(int x, int y)){
   int max = 0;
   int lci = -1;
   int rci = -1;
   while(index < len/2 ){ /* index starts at 0 */
 	lci = 2*(index+1) -1;
-	rci = 2*(index+1)+1 -1;
-	if(cmp(a[index],a[lci]) || cmp(a[index] , a[rci])){
-	  max = cmp(a[rci], a[lci]) ? lci : rci;
-	  swap(&a[index],&a[max]);
-	  maxheapify(a,len,max,cmp);	
-	}else{
+	rci = 2*(index+1) +1 -1;
+	if(ifChildPresent(lci,len) && cmp(a[index],a[lci]) || ifChildPresent(rci,len) && cmp(a[index] , a[rci])){ /* comparision function true means heap property satisfied, no need to swap and go down further*/
 	  break;
+	}else{
+	if(!ifChildPresent(rci,len))
+		max = lci;	
+	  else
+		max = cmp(a[rci], a[lci]) ? rci : lci;
+	  swap(&a[index],&a[max]);
+	  heapify(a,len,max,cmp);
 	}	 
   }
 }	
@@ -63,19 +73,22 @@ index? [hint what will the children of node 0?]
 
 */
 
-/* builds a max heap structure */
+/* builds an heap structure */
 
 void BuildHeap(int *a,int len, int (*cmp)(int x,int y)){
   for(int i = len/2; i >=0 ; i--){ /* leafs are already heap */
-	maxheapify(a,len,i,cmp);
+	heapify(a,len,i,cmp);
   }
 }
 
 void HeapSort(int *a , int len,int (*cmp)(int x,int y)){
   BuildHeap(a,len,cmp);
+  printArray(a,len);
   for(int i = len ; i > 0 ; i--){
-	swap(a[0],a[i]);/* put the max element at the end of array */
-	maxheapify(a,i-1,0,cmp); /* do heapify at index 0 but reduce the array size */
+	swap(a[0],a[i-1]);/* put the max element at the end of array */
+	cout<<"SWAP:" << " ";printArray(a,len);
+	heapify(a,i-1,0,cmp); /* do heapify at index 0 but reduce the array size */
+	printArray(a,len);
   }	
 }
 
@@ -83,9 +96,10 @@ int main(){
 
   int a[] = {3,4,5,0,1,2,3};
 
+  printArray(a,ARRAYSIZE(a));
+
   HeapSort(a,ARRAYSIZE(a),compare);
 
-  for(int i=0;i<ARRAYSIZE(a);i++){ // print
-	cout<<a[i]<< " ";
-  }
+  printArray(a,ARRAYSIZE(a));
+
 }
