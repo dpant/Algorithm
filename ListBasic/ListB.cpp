120e1891f2b6f626b15f728f3791e04757e67c65
dpant <dpant@gmail.com>
2013-08-15 16:41:23 -0700

    added iterator functionality initial version

--- ListBasic/ListB.cpp
+++ ListBasic/ListB.cpp
@@ -4,7 +4,8 @@
 using namespace std;
 
 class List{
-  class ListNode{
+
+  class ListNode{ /* remember inner class does not have access to outer class variable/funcs. if needed have to add ref to outer calss inside the inner class*/
   public:
 	int data;
 	ListNode* next;
@@ -12,15 +13,61 @@ class List{
   };
   ListNode *head, *tail;
   size_t msize;
-
+  /* to make iterator functionality work. we need to make end() which return iterator past the list */
+  ListNode *dummyendnode;  /* Notice dummyend can't be part of iterator, as we have to return same end for multiple iterator end() calls */
+	
 public:
 
-  List():head(NULL),tail(NULL),msize(0){
+  /* iterator implementation  */
+  class iterator{
+	ListNode *p;
+
+  public:
+	iterator(ListNode *ln):p(ln){
+
+	}
+	iterator():p(NULL){
+
+	}
+	int &operator *(){ 
+	  return p->data;
+	}
+	iterator& operator ++(){ //prefix
+	  p = p->next;
+	  return *this;
+	}
+	iterator& operator ++(int){ //prefix
+	  iterator toreturn = *this;
+	  p = p->next;
+	  return *this;
+	}
+	bool operator !=(const iterator &itr){
+	  return p != itr.p;
+	}
+	bool operator ==(const iterator &itr){
+	  return ! (*this != itr);
+	}
+
+  };
+
+  iterator begin(){
+	iterator *iobj = new iterator(head);
+	return *iobj;
   }
-  
-  size_t size(){return msize;
+  iterator end(){
+	iterator *iobj = new iterator(dummyendnode);
+    return *iobj;
   }
-  
+  /* constructors */
+  List():msize(0){
+	ListNode *ln = ListNode::MkNode();
+	head = tail = ln;
+	dummyendnode = ln;
+  }
+  size_t size(){
+	return msize;
+  }
+  /* member fucntions */
   void push_front(int i){
 	ListNode *ln = ListNode::MkNode();
 	ln->data = i;
@@ -34,14 +81,15 @@ public:
 
   int pop_front() throw(underflow_error){	/* exception specification. usually dont write it, its runtime garantee but might have performace hit due to non inlining of function */
 	int data;
-	if(msize == 0){ /* standard STL behaviour is undefined in case of underflow */
+	if(msize == 0){ /* STL behaviour is undefined in case of underflow */
 	  throw underflow_error("List already empty");
 	}
 	ListNode* tmp = head;
 	head = head->next;
 	msize--;	
 	if(msize == 0){
-	  tail = NULL;
+	  tail = dummyendnode;
+	  head = dummyendnode;
 	}
 	data = tmp->data;
 	delete tmp;
@@ -63,7 +111,7 @@ void myunexpected(){
   std::cerr <<"unexpected called\n";
   throw 0;
 }
-int main(){
+int testList(){
   List l = List();
   //List::ListNode lnd; /* can't do this.Class ListNode is  is private */
 
@@ -83,4 +131,18 @@ int main(){
   }
 
   l.printList();	
+}
+void iteratorTest(){
+  List l = List();
+  l.push_front(11);
+  l.push_front(22);
+  l.push_front(33);
+  l.push_front(44);
+  l.printList();
+  for(List::iterator i = l.begin();i != l.end(); i++){
+	cout << *i << endl;
+  }
+}
+int main(){
+  iteratorTest();
 }
