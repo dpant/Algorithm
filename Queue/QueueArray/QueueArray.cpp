6a55d78ff88a6b8d3226f2a601112c80aef7183c
dpant <dpant@gmail.com>
2013-08-18 12:41:19 -0700

    simplified the circular queue logic

--- Queue/QueueArray/QueueArray.cpp
+++ Queue/QueueArray/QueueArray.cpp
@@ -4,39 +4,40 @@
 
 At the begining when no item is there array looks like this.
 
-start+1 == end ==> queue is empty;
-
 Start
  |
 ---------------------------------------------
 	|	|	|	|	|	|	|	|	|	|	| 
 ---------------------------------------------
-	  |
-	End
 
-After two enqueue operation 
+
+After 3 enqueue operation 
 
 Start
  |
 ---------------------------------------------
-	| 2	| 7	|	|	|	|	|	|	|	|	| 
+ 1	| 2	| 7	|	|	|	|	|	|	|	|	| 
 ---------------------------------------------
-	  	      |
-		 	 End
+
+After 3 enqueue operation 
+
+	Start
+	 |
+---------------------------------------------
+ 	| 2	| 7	|	|	|	|	|	|	|	|	| 
+---------------------------------------------
+
 
 Several insert (Queue is Full)
 
-Start
- |
+	Start
+	 |
 ---------------------------------------------
-	| 2	| 7	| 3	| 3	| 2	| 1	| 6	| 5	| 3	| 2	| 
+ 11	| 2	| 7	| 3	| 3	| 2	| 1	| 6	| 5	| 3	| 2	| 
 ---------------------------------------------
- |
-End
 
 
-One cell is always empty !! that is how we can differentiate 
-between if queue if Full vs queue is empty
+
 
 */
 
@@ -48,36 +49,41 @@ private:
   size_t size;
   int* Array;
   int start;
-  int end;
+  int items;
 
   public:
   queueArray(size_t sizep){
-	size = sizep+1;
+	size = sizep;
 	Array = new int[size];
 	start = 0;
-	end = start + 1;
+	items = 0;
   }
   void enqueue(int i){
-	if(start == end){
+	size_t end;
+	if(items == size){
 	  cout << "Queue full" << endl;
 	  return ;
 	}
+	end = (start + items) % size;
 	Array[end] = i;
-	end = (end + 1) % size;
+	items++;
    
   }
   int dequeue(){
-	if(start+1 == end){
+	int fronte;
+	if(items == 0){
 	  cout<<"Queue Empty" << endl;
 	  return -1;
 	}
-	start = (start +1)% size;
-	return Array[start];
+	fronte = Array[start];
+	start = (start +1 )% size;
+	items-- ;
+	return fronte;
   }
 };
-int main(){
+int testQueue(){
   queueArray qA(10);
-  for(int i = 0; i < 10 ; i ++){
+  for(int i = 0; i < 11 ; i ++){
 	qA.enqueue(i);
   }
   for(int i = 0 ; i < 5 ; i ++){
@@ -90,5 +96,8 @@ int main(){
   for(int i = 0 ; i < 10 ; i ++){
 	cout << qA.dequeue() << " " ;
   }
-	
+}
+
+int main(){
+  testQueue();
 }
