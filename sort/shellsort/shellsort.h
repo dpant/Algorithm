630007a5d0c4677e223397537015b1b50bcd7361
dpant <dpant@gmail.com>
2013-08-20 22:15:17 -0700

    shell sort code cleanup

--- sort/shellsort/shellsort.h
+++ sort/shellsort/shellsort.h
@@ -1,18 +1,39 @@
 #include "../utils.h"
 #include <vector>
+#include <algorithm>
+#include <functional>
+#include <iostream>
+/* shell sort implemention. much faster than insertion and selection sort */
+/* 
+	Basic idea:
+	sort in different passes with distance d
+	no know perfect solution for d;
+	heuristic solution choose 2^i -1
+
+*/
 
 template <typename T>
 void shellsort(std::vector<T> &a, bool (*comp)(int ,int)){
-  for(int dist = a.size()/2; dist > 0 ; dist = dist/2){
-	for(int i = 0 ; i < dist; i++){
-	  int endv =  (a.size()) - ((a.size() -i ) % dist);
-	  for(int si = i ; si < a.size(); si = si + dist){
-		for(int end  = si ; end >= i + dist ; end = end - dist ){
-		  if(comp(a[end],a[end- dist])){
-			swap(a[end],a[end-dist]);
-		  }
+
+  std::vector<int> seq;			
+  for(int i=1;i< a.size();i = i*2+1 ){ /* generate the h sorting sequence highest to lowest */
+	seq.push_back(i);
+  }
+  std::sort(seq.begin(),seq.end(),std::greater<int>());
+
+  for(int dist = 0 ; dist < seq.size(); dist++){ /* distance sequence highest to lowest */
+	int distv = seq[dist];	
+
+	for(int i  = distv ; i < a.size() ; i++){ /* do h sort passes */
+	  for(int si = i ; si >= distv ; si = si - distv){/* now do h sort , go backwards.*/
+		if(comp(a[si],a[si - distv])){
+		  swap(a[si],a[si-distv]);
+		}else{
+		  break;
 		}
 	  }
 	}
-  } 
-}
+  }
+} 
+
+
--- sort/shellsort/test.cpp
+++ sort/shellsort/test.cpp
@@ -6,13 +6,15 @@
 using namespace std;
 int main(){
   vector<int> v;
-  for(int i =10; i > 0;i -- )
-	v.push_back(i);
+  //for(int i =10; i > 0;i -- )
+  //	v.push_back(i);
   GenerateTestFile("test.txt");	
   v = ReadTestFile("test.txt");
   shellsort(v,&comparef);
   for(int i=0; i< v.size();i++){
 	cout << v[i] <<endl;
   }
-  assert(validateSort(v,comparef));
+  if(!validateSort(v,comparef)){
+	std::cout<<"Mismatch. Incorrect sorting"<< std::endl;
+  }
 }
